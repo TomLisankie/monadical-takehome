3b83fcae4dfea7c1488c0baaf404058b164c741f
Thomas Lisankie <id@tomlisankie.com>
2021-04-05 22:24:22 -0500

    Building up channel layers so multiple clients can communicate

--- backend/topsy_turvy/consumers.py
+++ backend/topsy_turvy/consumers.py
@@ -2,14 +2,17 @@ import json
 from channels.generic.websocket import WebsocketConsumer
 
 class GameConsumer(WebsocketConsumer):
+    x = 0
     def connect(self):
+        game_id = self.scope["url_route"]["kwargs"]["game_id"]
         self.accept()
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
+        self.x += 1
         self.send(text_data=json.dumps({
-            'json_received' : text_data_json
+            'x' : self.x
         }))
 
     def disconnect(self, close_code):
-        pass
+        print("Disconnected with code: " + str(close_code))
--- backend/topsy_turvy/routing.py
+++ backend/topsy_turvy/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"game/(?P<game_id>\w+)/$", consumers.GameConsumer.as_asgi())
+    re_path(r"game/(?P<game_id>[0-9a-f-]+)", consumers.GameConsumer.as_asgi())
 ]
